67821977924180ae4f3c2706cc67dcd5a8c92f8b
menghonglin <mhlplus@outlook.com>
2021-05-23 16:18:17 +0800

    chore:去掉idea的配置文件

--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -13,8 +13,7 @@ export const RegisterScreen = () => {
     return (
         <Form onFinish={handleSubmit}>
             <Form.Item name={'username'} rules={[{required: true, message: '请输入用户名'}]}>
-                <Input placeholder={'请输入用户名'} type={'text'} id={'username'}/>
-            </Form.Item>
+   <Input placeholder={'请输入用户名'} type={'text'} id={'username'}/> </Form.Item>
             <Form.Item name={'password'} rules={[{required: true, message: '请输入密码'}]}>
                 <Input placeholder={'请输入密码'} type={'password'} id={'password'}/>
             </Form.Item>
